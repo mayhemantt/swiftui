7786feab1af49ea3ae1d3ce9afd48d3697b603ff
Hemant Joshi <mailhemantjoshi@gmail.com>
2021-05-19 15:25:54 +0530

    Initial Commit

---
+++ designcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  designcode
+//
+//  Created by Mayank Joshi on 19/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ designcode/designcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  designcodeApp.swift
+//  designcode
+//
+//  Created by Mayank Joshi on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct designcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
